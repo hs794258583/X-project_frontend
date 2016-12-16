1f92300cc9de7e9518c0852c328785c12af38352
Linh Ho <homautuanlinh@gmail.com>
2016-12-16 10:14:45 +0700

    add review model

---
+++ XFront/src/app/model/review.model.ts
@@ -0,0 +1,10 @@
+export interface Review {
+    "ReviewId": number,
+    "ReviewTitle": string,
+    "ReviewContent": string,
+    "CreatedDate": string,
+    "LastEditedDate": string,
+    "UserId": string,
+    "Rating": number,
+    "Slug": string
+}
